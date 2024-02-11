2fa44fc57bc6d29e4640ab646a5a7ef2e671a520
Tzeyi <tytiong2@gmail.com>
2024-02-11 12:35:59 -0500

    continuation from last yeear

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    public TextMeshProUGUI waveCountdownText;
+
+    private int waveNumber = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        waveNumber++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
